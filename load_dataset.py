1cf28c99d600defd53bd77c35152a4e750b3dc3a
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-11-15 12:36:29 +0100

    Add separate functions for loading whole dataset and training, validation pair

--- load_dataset.py
+++ load_dataset.py
@@ -1,22 +1,42 @@
-from tensorflow.keras.preprocessing import image_dataset_from_directory
+from typing import Tuple
 from functools import partial
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+
+def load_all_dataset(directory: str,
+                     batch_size=16,
+                     img_size=(640, 640),
+                     seed=None) -> tf.data.Dataset:
+    return image_dataset_from_directory(
+        directory=directory,
+        batch_size=batch_size,
+        image_size=img_size,
+        seed=seed
+    )
+
+
+def load_partial_dataset(directory: str,
+                         batch_size=16,
+                         img_size=(640, 640),
+                         seed=666,
+                         validation_split=0.2) \
+        -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+    load_dataset = partial(
+        image_dataset_from_directory,
+        directory=directory,
+        validation_split=validation_split,
+        seed=seed,
+        image_size=img_size,
+        batch_size=batch_size
+    )
+    return load_dataset(subset="training"), load_dataset(subset="validation")
 
 
 def main():
     img_path = "../images/train"
-    img_size = (640, 640)
-    batch_size = 16
-
-    load_dataset = partial(
-        image_dataset_from_directory,
-        directory=img_path,
-        validation_split=0.2,
-        seed=666,
-        image_size=img_size,
-        batch_size=batch_size
-    )
-    train_dataset = load_dataset(subset="training")
-    validation_dataset = load_dataset(subset="validation")
+    train_dataset, validation_dataset = load_partial_dataset(img_path)
+    dataset = load_all_dataset(img_path)
 
 
 if __name__ == "__main__":
