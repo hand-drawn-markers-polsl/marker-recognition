b2940f97fb4e378ad5e38be6330d4fe4d9b9a69d
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-11-30 23:41:22 +0100

    Add plotting heatmaps for multiple images

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import glob
+import os
 
 from make_models import make_simple_cnn, save_model
 from train import train
@@ -36,7 +38,14 @@ if __name__ == '__main__':
     }
 
     model = make_simple_cnn()
-    img_path = "../images/train/true/IMG_6605.jpg"
     train(model, params)
-    save_model(model, "dupa")
-    plot_heatmaps(img_path, params['img_size'], model, "dupa.jpg")
+    save_model(model, "first_working_model")
+
+    path_pattern = f"{params['img_directory']}/**/*.jpg"
+    base_dir = "heatmaps"
+    if not os.path.exists(base_dir):
+        os.mkdir(base_dir)
+    for i, img_path in enumerate(glob.glob(path_pattern, recursive=True)):
+        print(f"Iteration: {i}")
+        img_name = f"{base_dir}/{os.path.basename(img_path)}"
+        plot_heatmaps(img_path, params['img_size'], model, img_name)
