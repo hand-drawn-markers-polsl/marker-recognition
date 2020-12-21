a0dad52c0b3c6d5dc3e24dc5bd02a45f39c966c0
Maciej Ziaja <maciejzjg@gmail.com>
2020-12-21 21:39:40 +0100

    Add extra quotes in sh script and comment

--- convert_imgs.sh
+++ convert_imgs.sh
@@ -3,7 +3,9 @@
 # This script helps to fix invalid photos shot on some phones
 # (e.g. on iPhone XR)
 
+# This globs for "m*" because currently "mz" prefixed directories
+# include photos that need fixing.
 for i in data/raw/m*/**/*.jpg; do
-  echo Converting $i
+  echo Converting "$i"
   magick "$i" "$i"
 done
